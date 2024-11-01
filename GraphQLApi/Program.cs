59637f37ab9e859953d44c7acd9bde06afc2b7fd
Luis Alvarenga <l.alvarengav@gmail.com>
2024-11-01 06:45:25 -0600

    feat(database): add initial database migration and auto-migrate on startup

--- GraphQLApi/Program.cs
+++ GraphQLApi/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+using PizzaOrders.GraphQLApi.Data;
 using PizzaOrders.GraphQLApi.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,6 +7,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddApplicationServices();
 builder.Services.AddDatabaseConfiguration(builder.Configuration);
 builder.Services.AddGraphQLConfiguration();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("DefaultConnection"))
+    )
+);
+
 
 var app = builder.Build();
 
